db647392ddc0bd4987d17a36059470b73b3e311f
anasselghazi <anasseelghazi10@gmail.com>
2025-12-23 22:22:50 +0100

    feat:ajouter class utilisateur avec les sont functions

--- classes/utilisateur.php
+++ classes/utilisateur.php
@@ -1,1 +1,163 @@
+ <?php
+
+
+require_once "database.php";
+
+
+class utilisateur {
+
+private $id ;
+private $nom ;
+private $email ;
+private $role;
+private $motpasshash;
+private $etat;
+private $approuve ;
  
+ public function  __construct($id = null,$nom = null,$email = null,$role = null,$motpasshash = null,
+ $etat=1,$approuve=1)
+{
+$this->id = $id;
+$this->nom=$nom;
+$this->email = $email;
+$this->role = $role ;
+$this->motpasshash=$motpasshash;
+$this->etat=$etat;
+$this->approuve=$approuve;
+}
+
+/*getters*/
+
+public function getId(){
+    return $this-> id;
+}
+public function getNom(){
+return $this-> nom ;
+}
+public function getEmail(){
+    return $this-> email;
+}
+public function getRole(){
+    return $this->role;
+}
+public function getMotpasshash(){
+    return $this->motpasshash;
+}
+public function getEtat(){
+    return $this->etat;
+}
+public function getApprouve(){
+    return $this->approuve;
+}
+
+/*setters*/
+
+public function setId($id){
+    $this->id=$id;
+}
+public function setNom($nom){
+    $this->nom=$nom;
+}
+public function setEmail($email){
+    $this->email=$email;
+}
+public function setRole($role){
+    $this->role=$role;
+}
+public function setMotpasshash($motpasshash){
+$this->motpasshash=$motpasshash;
+}
+public  function setEtat($etat){
+    $this->etat=$etat;
+}
+public function setApprouve($approuve){
+    $this->approuve=$approuve;
+}
+public function cree(){
+
+$db= new database();
+
+$pdo = $db->getPdo();
+
+
+$sql = "INSERT INTO utilisateurs(nom,email,role,
+motpasse_hash,etat,approuve)
+VALUES(:nom,:email,:role,:motpasshash,:etat,:approuve)";
+
+ $stmt = $pdo->prepare($sql);
+
+$resultat = $stmt->execute([
+ ':nom' => $this->nom,':email' => $this->email,
+ ':role' => $this->role,':motpasshash' => $this->motpasshash,
+ ':etat' => $this->etat,':approuve' => $this->approuve
+        ]);
+
+
+if ($resultat) {
+$this->id = $pdo->lastInsertId();
+ return true;
+ }
+ return false;
+
+
+}
+
+ 
+
+
+
+public function trouverParEmail($email) {
+    
+$db= new database();
+
+$pdo = $db->getPdo();
+    
+    
+    $sql = "SELECT * FROM utilisateurs WHERE email = :email LIMIT 1";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':email' => $email]);
+    
+    
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    
+    if ($data) {
+        return new utilisateur(
+            $data['id'],
+            $data['nom'],
+            $data['email'],
+            $data['role'],
+            $data['motpasse_hash'],
+            $data['etat'],
+            $data['approuve']
+        );
+    }
+    
+    
+    return null;
+}
+
+ 
+
+
+public function verifierMotDePasse($motpass) {
+    
+    return password_verify($motpass, $this->motPasseHash);
+}
+
+
+
+
+
+
+
+
+
+}
+ 
+ 
+
+
+ 
+
+ ?>
