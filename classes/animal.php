d533d84e1db479380330a22ab18054fe55b77479
anasselghazi <anasseelghazi10@gmail.com>
2026-01-07 10:40:07 +0100

    feat:fix les problemes

--- classes/animal.php
+++ classes/animal.php
@@ -23,25 +23,56 @@ class Animal {
         $this->id_habitat    = $id_habitat;
     }
 
-    //  TES GETTERS 
-    public function getId() { return $this->id_animal; }
-    public function getNom() { return $this->nom; }
-    public function getEspece() { return $this->espece; }
-    public function getAlimentation() { return $this->alimentation; }
-    public function getImage() { return $this->image; }
-    public function getPaysOrigine() { return $this->pays_origine; }
-    public function getDescription() { return $this->description; }
-    public function getIdHabitat() { return $this->id_habitat; }
+    //   GETTERS 
+    public function getId() {
+         return $this->id_animal;
+         }
+    public function getNom() { 
+        return $this->nom; 
+    }
+    public function getEspece() { 
+        return $this->espece; 
+    }
+    public function getAlimentation() { 
+        return $this->alimentation; 
+    }
+    public function getImage() { 
+        return $this->image; 
+    }
+    public function getPaysOrigine() {
+         return $this->pays_origine; 
+        }
+    public function getDescription() {
+         return $this->description; 
+        }
+    public function getIdHabitat() { 
+        return $this->id_habitat; 
+    }
 
-    //  TES SETTERS 
-    public function setId($id) { $this->id_animal = $id; }
-    public function setNom($nom) { $this->nom = $nom; }
-    public function setEspece($espece) { $this->espece = $espece; }
-    public function setAlimentation($alimentation) { $this->alimentation = $alimentation; }
-    public function setImage($image) { $this->image = $image; }
-    public function setPaysOrigine($pays_origine) { $this->pays_origine = $pays_origine; }
-    public function setDescription($description) { $this->description = $description; }
-    public function setIdHabitat($id_habitat) { $this->id_habitat = $id_habitat; }
+    //   SETTERS 
+    public function setId($id) { 
+        $this->id_animal = $id; 
+    }
+    public function setNom($nom) { 
+        $this->nom = $nom; 
+    }
+    public function setEspece($espece) { 
+        $this->espece = $espece;
+     }
+    public function setAlimentation($alimentation) {
+         $this->alimentation = $alimentation; 
+        }
+    public function setImage($image) { $this->image = $image;
+     }
+    public function setPaysOrigine($pays_origine) {
+         $this->pays_origine = $pays_origine; 
+        }
+    public function setDescription($description) {
+         $this->description = $description; 
+        }
+    public function setIdHabitat($id_habitat) {
+         $this->id_habitat = $id_habitat; 
+        }
 
     // TES METHODES EXISTANTES 
     public static function listerTous() {
